ee15d4641fb0a95d09d283d0d837c1838ccb2a11
Narendra Nath Joshi <narendranathjoshi@gmail.com>
2016-02-21 08:07:06 +0530

    init and push and commit

--- client/initialize.py
+++ client/initialize.py
@@ -5,20 +5,20 @@ import os
 '''
     ocsm init comes here! 
 '''
+
+
 def initialize():
     add_gitignore()
     status = generate.generate()
-    if status["code"] ==0:
+    if status["code"] == 0:
         print "ERROR: could not generate config.oscm, " + str(status.error_desc)
         return 0
-    #oscm_prompt.prompt()
+    # oscm_prompt.prompt()
     return 1
 
+
 def add_gitignore():
-    p = os.path.join(os.getcwd(),".gitignore")
-    fp = open(p,"w")
+    p = os.path.join(os.getcwd(), ".gitignore")
+    fp = open(p, "w")
     fp.write("*/")
     fp.close()
-
-if __name__ == '__main__':
-    print str(init())
--- client/oscm.py
+++ client/oscm.py
@@ -1,3 +1,4 @@
+import os
 import click
 from click.testing import CliRunner
 import requests
@@ -13,80 +14,97 @@ put = "put/?"
 get = "get/?"
 param_file = "my_file"
 
+
 @click.group()
 def cli():
     pass
 
+
 @cli.command()
 @click.option('--name', '-n', 'name',
-    prompt = 'Enter the software name',
-    help='The software name',
-    required=True)
+              prompt='Enter the software name',
+              help='The software name',
+              required=True)
 @click.option('--version', '-v', 'version',
-    prompt = 'Enter the software version',
-    help='Version number, not version name',
-    required=True)
+              prompt='Enter the software version',
+              help='Version number, not version name',
+              required=True)
 @click.option('--url', '-u', 'url',
-    prompt = 'Enter the download url, press n for none',
-    help = 'The url which points to download binary for a particular OS and architecture.')
+              prompt='Enter the download url, press n for none',
+              help='The url which points to download binary for a particular OS and architecture.')
 @click.option('--os', '-o', 'os',
-    prompt = 'Enter the os for which the url/command is valid',
-    help = 'Operating System name; darwin for macs, linux and windows; for which \
+              prompt='Enter the os for which the url/command is valid',
+              help='Operating System name; darwin for macs, linux and windows; for which \
         for which the download url/command is pointing to.',
-    required=True)
+              required=True)
 @click.option('--cmd', '-c', 'cmd',
-    prompt = 'Enter the install command, press n for none',
-    help = 'The install command like npm module name or \"apt-get xyz\" or \"pip install xyz\"')
+              prompt='Enter the install command, press n for none',
+              help='The install command like npm module name or \"apt-get xyz\" or \"pip install xyz\"')
 @click.option('--tag', '-t', 'tag',
-    prompt = 'Enter the tag for software, enter comma seperated tags for multiple values',
-    help = 'Tags are used to help prompt for softwares under a category\n \
+              prompt='Enter the tag for software, enter comma seperated tags for multiple values',
+              help='Tags are used to help prompt for softwares under a category\n \
             Example : JDK, JRE etc will come under java tag\n \
             Enter multiple tags by seperating them with commas, making sure no\
             space is added before or after the comma.')
 @click.option('--architecture', '-a', 'arch',
-    prompt = 'Enter the architecture for which the download url/command is pointing to.\n \
+              prompt='Enter the architecture for which the download url/command is pointing to.\n \
             Enter `both` for both x86 and 64 bit., 32 for 32 bit and 64 for 64 bit.',
-    help = '32 bit or 64 bit; x86 is 32 bit.',
-    required=True)
-def addsoftware(name=None, version=None, url=None, os=None, cmd=None, tag=None, arch=None):
+              help='32 bit or 64 bit; x86 is 32 bit.',
+              required=True)
+def addsoftware(name=None, version=None, url=None, os=None, cmd=None, tag=None,
+                arch=None):
     add_software(name, version, url, os, cmd, tag, arch)
 
-def add_software(name=None, version=None, url=None, os=None, cmd=None, tag=None, arch=None):
+
+def add_software(name=None, version=None, url=None, os=None, cmd=None, tag=None,
+                 arch=None):
     global endpoint_url, put, param_file
     endpoint = endpoint_url + put
-    params = {"name": name, "version": version, "os": os, "arch": arch, "command": cmd, "url": url, "tag": tag}
-    params = dict((k,v) for k,v in params.items() if v.lower() != 'n')
-    response = requests.get("%s%s" %(endpoint, urllib.urlencode(params)))
-    print_status(response) 
+    params = {"name": name, "version": version, "os": os, "arch": arch,
+              "command": cmd, "url": url, "tag": tag}
+    params = dict((k, v) for k, v in params.items() if v.lower() != 'n')
+    response = requests.get("%s%s" % (endpoint, urllib.urlencode(params)))
+    print_status(response)
     print params
 
     # save to file:
     with open(param_file, 'w') as f:
         json.dump(params, f)
 
+
 @cli.command()
-def init():
+@click.option('--url', '-u', 'url',
+              prompt='Enter the repo URL',
+              help='The repo clone URL')
+def init(url):
     initialize.initialize()
+    subprocess.call("git init", shell=True)
+    subprocess.call("git remote add origin %s" % url, shell=True)
+
+
 
 @cli.command()
 @click.option('--repo', '-r', 'repo',
               prompt='Enter the repo clone URL',
               help='The repo clone URL')
 def pull(repo):
+
     pull_repo.pull(repo)
 
+
 @cli.command()
-@click.option('--repo', '-r', 'repo',
-              help='The repo clone URL')
 def push(repo):
-    pull_repo.pull(repo)
+    subprocess.call("git push", shell=True)
+
 
 @cli.command()
-@click.option('--repo', '-r', 'repo',
-              prompt='Enter the repo clone URL',
-              help='The repo clone URL')
-def commit(repo):
-    pull_repo.pull(repo)
+@click.option('--message', '-m', 'message',
+              prompt='Enter the commit message',
+              help='The commit message')
+def commit(message):
+    subprocess.call("git add conf/config.oscm", shell=True)
+    subprocess.call("git commit -m '%s'" % message, shell=True)
+
 
 def tester():
     call_command('addsoftware')
@@ -100,19 +118,21 @@ def tester():
             data = {}
     print data
 
+
 def print_status(response):
     if response.status_code == 201:
         click.echo("Software added successfully.")
     else:
         click.echo("Software wasn't added. Network error.")
 
+
 def call_command(cmdname):
     '''runner = CliRunner()
     result = runner.invoke(cli, [cmdname])'''
-    cmdname = 'oscm '+cmdname
-    #result,error = subprocess.Popen(cmdname,stdout = subprocess.PIPE, stderr= subprocess.PIPE).communicate()
+    cmdname = 'oscm ' + cmdname
+    # result,error = subprocess.Popen(cmdname,stdout = subprocess.PIPE, stderr= subprocess.PIPE).communicate()
     subprocess.call(cmdname, shell=True)
-    
+
     '''if isinstance(result, six.string_types):
         return result.output.strip()    
     else:
