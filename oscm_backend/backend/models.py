f92e11423536278e4504cb8142f4c4af8d923559
Narendra Nath Joshi <narendranathjoshi@gmail.com>
2016-02-21 08:34:02 +0530

    os all

--- oscm_backend/backend/models.py
+++ oscm_backend/backend/models.py
@@ -7,7 +7,9 @@ from django.db import models
 class SoftwareConfiguration(models.Model):
     OS_DARWIN = 0
     OS_LINUX = 1
+    OS_ALL = 2
     OS_CHOICES = (
+        (OS_ALL, "OS_ALL"),
         (OS_DARWIN, "OS_DARWIN"),
         (OS_LINUX, "OS_LINUX"),
     )
@@ -23,7 +25,7 @@ class SoftwareConfiguration(models.Model):
 
     name = models.CharField(max_length=500, blank=True, null=True)
     version = models.CharField(max_length=100, blank=True, null=True)
-    os = models.IntegerField(choices=OS_CHOICES, default=OS_DARWIN)
+    os = models.IntegerField(choices=OS_CHOICES, default=OS_ALL)
     arch = models.CharField(max_length=10, choices=ARCH_CHOICES, default=ARCH_64)
     command = models.CharField(max_length=500, blank=True, null=True)
     url = models.URLField(max_length=500, blank=True, null=True)
